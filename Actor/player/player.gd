c411f94d0a2f36d9e4377f747a26e61fddd91f9d
Valonekiil <149128324+Valonekiil@users.noreply.github.com>
2025-09-13 16:57:50 +0700

    buff hold & spawn done

--- Actor/player/player.gd
+++ Actor/player/player.gd
@@ -1,14 +1,29 @@
 extends CharacterBody2D
 
 @export var speed: float = 300.0
+@onready var txt:Label = $Label
+@onready var sprite_buff = $buff
 
 var collect_buffs:Array[Buff_Drop]
+var near_knight:bool
 var cur_buff:Buff_Data 
+
+func _ready() -> void:
+	txt.visible = false
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("grab"):
+		if cur_buff and near_knight:
+			print("memberikan buff")
+			give_buff(cur_buff)
+		elif !cur_buff and not collect_buffs.is_empty():
+			print("mengambil buff")
+			grab_buff()
+		else:
+			print("gak ngapa ngapain")
 		if cur_buff:
-			grab_buff(cur_buff)
+			print("hold buff sekarang " + cur_buff.name)
+
 
 func _physics_process(delta: float) -> void:
 	# Get input direction
@@ -35,14 +50,20 @@ func Collecting_Buff():
 	if not collect_buffs.is_empty():
 		var buffs:Buff_Drop = collect_buffs.get(0)
 		buffs.hide_label()
-		cur_buff = buffs.Buff
 		buffs.show_label()
 
-func grab_buff(buff:Buff_Data):
-	GlobalSignal.emit_signal("activate_buff",cur_buff)
+func grab_buff():
 	var buff_node = collect_buffs.get(0)
+	print("mengambil buff " + buff_node.Buff.name)
 	collect_buffs.erase(buff_node)
+	cur_buff = buff_node.Buff
+	sprite_buff = buff_node.Text
 	buff_node.queue_free()
+
+func give_buff(buff:Buff_Data):
+	GlobalSignal.emit_signal("activate_buff",cur_buff)
+	cur_buff = null
+	txt.visible = false
 
 func _on_colect_area_entered(area: Area2D) -> void:
 	if area.get_parent() is Buff_Drop:
@@ -55,5 +76,15 @@ func _on_colect_area_exited(area: Area2D) -> void:
 		var buff = area.get_parent()
 		collect_buffs.erase(buff)
 		buff.hide_label()
-		cur_buff = null
 		Collecting_Buff()
+
+func _on_colect_body_entered(body: Node2D) -> void:
+	if body.is_in_group('knight'):
+		near_knight = true
+		if near_knight:
+			txt.visible = true
+
+func _on_colect_body_exited(body: Node2D) -> void:
+	if body.is_in_group('knight'):
+		near_knight = false
+		txt.visible = false
--- Buff/drop/buff_drop.gd
+++ Buff/drop/buff_drop.gd
@@ -2,23 +2,21 @@ extends Node2D
 class_name Buff_Drop
 
 @export var Buff:Buff_Data
-@onready var Text = $Sprite2D
+@onready var Text:Sprite2D = $Sprite2D
 @onready var Area = $Area2D
+@onready var col:CollisionShape2D = $Area2D/CollisionShape2D
 @onready var Collect = $Label
+
+func init(buff:Buff_Data):
+	Buff = buff
 
 func _ready() -> void:
 	Collect.hide()
+	Text.texture = Buff.sprite
+	col.shape.radius = Buff.sprite.get_height()
 
 func show_label():
 	Collect.show()
 
 func hide_label():
 	Collect.hide()
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group('Player'):
-		pass
-
-
-func _on_area_2d_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
